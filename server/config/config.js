fb6de892f8533421ba959cfbfc2d6cc695e39d9d
matifalcone <matias.falcone@gmail.com>
2021-08-18 17:28:18 +1000

    Redis and new functionality

--- server/config/config.js
+++ server/config/config.js
@@ -5,4 +5,10 @@ process.env.PORT = process.env.PORT || 3001;
 process.env.NODE_ENV = process.env.NODE_ENV || "dev"
 
 // API-KEY
-process.env.API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+process.env.API_KEY = "BQY5wayTu6KZOqVhwqhGLGBHqQfxPTKV";
+
+// API-KEY-THE-GRAPH
+process.env.API_KEY_THE_GRAPH = "20d004b48e2d0526d5f46aacdf18e80f";
+
+// REDIS
+process.env.REDIS_URL = process.env.REDIS_URL || { "port": 6379, "host": "localhost" }
