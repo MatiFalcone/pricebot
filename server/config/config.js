0bb97e4dc63e61c29622c18d56a30f1d7531cf48
matifalcone <matias.falcone@gmail.com>
2021-08-19 08:32:29 +1000

    local redis

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,3 @@ process.env.API_KEY = "BQY5wayTu6KZOqVhwqhGLGBHqQfxPTKV";
 
 // API-KEY-THE-GRAPH
 process.env.API_KEY_THE_GRAPH = "20d004b48e2d0526d5f46aacdf18e80f";
-
-// REDIS
-process.env.REDIS_URL = process.env.REDIS_URL || { "port": 6379, "host": "localhost" }
